0f13e3df000a1719b1b57d9b577c7346f99deff9
Dan D <degeare28@gmail.com>
2020-03-25 14:29:31 -0600

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dbUser:Theodog28!@task-master-queyp.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    validate: [
+      ({ length }) => length >= 8,
+      "Password should be at least 8 characters."
+    ],
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+module.exports = User = mongoose.model("users", UserSchema);
